302b03016f3827a49fa5f6ea7da986875b9d92f5
@nikita_akulionok <mikita0511@gmail.com>
2025-09-26 16:56:05 +0300

    Ready

--- services/i18n/middleware.py
+++ services/i18n/middleware.py
@@ -1,44 +1,4 @@
 # ## i18n/middleware.py
-# from __future__ import annotations
-# from typing import Any, Awaitable, Callable, Dict, Optional
-# from aiogram import BaseMiddleware
-# from aiogram.types import Update
-# from .translations import Translator
-#
-# def _extract_lang_from_update(update: Update) -> Optional[str]:
-#     u = update
-#     if u.message and u.message.from_user: return u.message.from_user.language_code
-#     if u.callback_query and u.callback_query.from_user: return u.callback_query.from_user.language_code
-#     if u.inline_query and u.inline_query.from_user: return u.inline_query.from_user.language_code
-#     if u.my_chat_member and u.my_chat_member.from_user: return u.my_chat_member.from_user.language_code
-#     if u.chat_member and u.chat_member.from_user: return u.chat_member.from_user.language_code
-#     return None
-
-# class LocaleMiddleware(BaseMiddleware):
-#     def __init__(self, translator: Translator, get_user_locale: Callable[[int], Optional[str]] | None = None):
-#         self.tr = translator
-#         self.get_user_locale = get_user_locale
-#
-#     async def __call__(self, handler: Callable[[Any, Dict[str, Any]], Awaitable[Any]],
-#                        event: Update, data: Dict[str, Any]) -> Any:
-#         tg_lang = _extract_lang_from_update(event)
-#         user_id = (
-#             event.message.from_user.id if event.message and event.message.from_user else
-#             event.callback_query.from_user.id if event.callback_query and event.callback_query.from_user else
-#             None
-#         )
-#         saved = None
-#         if self.get_user_locale and user_id:
-#             try: saved = self.get_user_locale(int(user_id))
-#             except Exception: saved = None
-#
-#         loc = self.tr.pick_locale(saved, tg_lang)
-#         data["locale"] = loc
-#         data["tr"] = self.tr.for_locale(loc)
-#         data["trn"] = self.tr.for_locale_plural(loc)
-#         return await handler(event, data)
-
-
 from __future__ import annotations
 
 from typing import Any, Awaitable, Callable, Dict, Optional
